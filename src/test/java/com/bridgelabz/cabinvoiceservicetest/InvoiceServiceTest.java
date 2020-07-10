7e0880f94e2916ae4ac50db993d5f393b765a042
Amrut <amrutpanda7789@gmail.com>
2020-07-10 16:46:06 +0530

    ADDED: Given distance and time should return total fare for cab service

---
+++ src/test/java/com/bridgelabz/cabinvoiceservicetest/InvoiceServiceTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.cabinvoiceservicetest;
+
+import com.bridgelabz.cabinvoiceservice.InvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+
+    InvoiceGenerator invoiceGenerator;
+
+    @Before
+    public void create_Object_Of_InvoiceGenerator() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+}
